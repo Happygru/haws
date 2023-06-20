cf4fb13b1748d113925298a0ad3947758b21e8a9
Happygru <130092372+Happygru@users.noreply.github.com>
2023-06-20 08:45:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 function App() {
   var settings = {
